e6e24a63c11da2c1ecfe280b1db4a3a9b7722d20
Igor Viana <igorvianacont@gmail.com>
2026-01-27 22:19:34 -0300

    feat: Creating schemas and configuring handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/IgorVianadF/goLang/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func Init() {
+	logger = config.GetLogger("Handler")
+	db = config.GetDB()
+}
--- schemas/drawing.go
+++ schemas/drawing.go
@@ -9,8 +9,20 @@ import (
 type Drawing struct {
 	gorm.Model
 	Name      string
-	Price     int64
+	Price     *float64
 	Customer  string
 	StartDate time.Time
-	EndDate   time.Time
+	EndDate   *time.Time
+}
+
+type DrawingResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitzero"`
+	Name      string    `json:"name"`
+	Price     float64   `json:"price"`
+	Customer  string    `json:"customer"`
+	StartDate time.Time `json:"startDate"`
+	EndDate   time.Time `json:"endDate"`
 }
